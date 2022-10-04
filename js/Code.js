861722b6cd54aa1861bd7350b97a866c39e9d3e1
Arhat2468 <95970247+Arhat2468@users.noreply.github.com>
2022-10-04 17:41:26 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "TRUE";
+const accessCode2 = "TRUE";
+const accessCode3 = "FALSE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("TRUE or FALSE", 100,50)
+    fill("yellow")
+    text("Q-1: Cot A is not defined for A = 0 !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("TRUE or FALSE", 670,150)
+    fill("yellow")
+    text("Q-2: The value of Sin θ increase as θ increases !!", 670,170)
+
+    fill("white")
+    textSize(15)
+    text("TRUE or FALSE", 100,250)
+    fill("yellow")
+    text("Q-3: The value of Cos θ increases as θ increase !!", 100,270)
+
+}
